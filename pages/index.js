f674c7fdd99b45c331a2b25a13b7b973e54a62b9
John Michael De Guzman <jmxdg1029@gmail.com>
2021-06-19 05:39:31 -0400

    fixed deployment bug

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({filteredCoins}) {
         <CoinList filteredCoins={filteredCoins}/>
       </main>
 
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"></link>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossOrigin="anonymous"></link>
     </div>
   )
 }
