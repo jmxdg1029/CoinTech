0a53defd51b03350b0a24afd712e4ef766712df2
John Michael De Guzman <jmxdg1029@gmail.com>
2021-06-21 01:29:28 -0400

    deployed line graph for coins

--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -1,78 +1,109 @@
 import Layout from '../../components/Layout';
 import Image from 'next/image'
 import style from './Coin.module.css'
-import React, {useRef, useEffect} from 'react';
-
+import React, {useRef, useEffect, useState} from 'react';
+import { chartConfigs } from './chartConfigs/chartConfigs';
 import Chartjs from "../../node_modules/chart.js"
+import coinGecko from '../../apis/coinGecko';
 
-const Coin = ({coin}) => {
+
+const Coin = ({coin,id}) => {
     const chartRef = useRef()
+    const [coinData, setCoinData] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    const formatData = (data) => {
+        return data.map((el) =>{
+            return {
+                t: el[0],
+                y: el[1].toFixed(2),
+            };
+        });
+    };
+    
+ 
+    useEffect(() => {
+    const fetchData = async () => {
+        setIsLoading(true);
+        const [day,week] = await Promise.all([
+            coinGecko.get(`/coins/${id}/market_chart/`,{
+                params:{
+                    vs_currency: "cad",
+                    days: "1",
+                },
+            }),
+           
+            coinGecko.get(`/coins/${id}/market_chart/`,{
+                params:{
+                    vs_currency: "usd",
+                    days: "7",
+                },
+            }),
+        ]);
+       
+        setCoinData({
+            day: formatData(day.data.prices),
+            week: formatData(week.data.prices)
+        });
+        setIsLoading(false);
+        
+    };
+    fetchData();
+    }, []);
 
     useEffect(() => {
         if(chartRef && chartRef.current){
             const chartInstance = new Chartjs(chartRef.current, {
                 type: 'line',
                 data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                     datasets: [{
                         label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
+                        data: coinData.day,
+                        backgroundColor: "rgba(174,305,194,0.5)",
+                        pointRadius:0,
                     }]
                 },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
+                options: chartConfigs
             });
-        }
-    })
+        };
+    });
+
     return (
         <Layout>
             <div className={style.coin_container}>
-                <canvas ref={chartRef} id="myChart" width={250} height={250}></canvas>
+                
+                <div className={style.coin_chart}>
+                    <canvas ref={chartRef} id="myChart" width={550} height={550}></canvas>
+                </div>
                 <div className={style.coin_desc}>
-                    <Image  src={coin.image.large} alt={coin.name} height={300} width={300} />
+                    <Image  src={coin.image.large} alt={coin.name} height={200} width={200} />
                     <p>{coin.name}</p>
                     <p>{coin.symbol}</p>
-                    <p>${coin.market_data.current_price.cad}</p>
+                    <p>Current Price: ${coin.market_data.current_price.cad}</p>
+               
                 </div>
             </div>
         </Layout>
     )
 };
 
-export default Coin
+
 
 export async function getServerSideProps(context) {
     const {id} = context.query
 
     const res = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
+    
 
     const data = await res.json()
+    
 
     return {
         props: {
-            coin: data
+            coin: data,
+            id
         }
     }
 }
+
+export default Coin
