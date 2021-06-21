197f7bd2a5165eabd76b8511d253423f0bb14492
John Michael De Guzman <jmxdg1029@gmail.com>
2021-06-21 04:05:40 -0400

    chart buttons

--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -10,6 +10,21 @@ const Coin = ({coin,id}) => {
     const chartRef = useRef()
     const [coinData, setCoinData] = useState({});
     const [isLoading, setIsLoading] = useState(false);
+    const [timeFormat, setTimeFormat] = useState("24h")
+
+    const determineTimeFormat = () => {
+        switch(timeFormat){
+            case "24h":
+                return coinData.day;
+            case "7d":
+                return coinData.week;
+            case "1y":
+                return coinData.year;
+            default:
+                return coinData.day;
+                    
+        }
+    }
 
     const formatData = (data) => {
         return data.map((el) =>{
@@ -24,7 +39,7 @@ const Coin = ({coin,id}) => {
     useEffect(() => {
     const fetchData = async () => {
         setIsLoading(true);
-        const [day,week] = await Promise.all([
+        const [day,week,year] = await Promise.all([
             coinGecko.get(`/coins/${id}/market_chart/`,{
                 params:{
                     vs_currency: "cad",
@@ -34,15 +49,22 @@ const Coin = ({coin,id}) => {
            
             coinGecko.get(`/coins/${id}/market_chart/`,{
                 params:{
-                    vs_currency: "usd",
+                    vs_currency: "cad",
                     days: "7",
+                },
+            }),
+            coinGecko.get(`/coins/${id}/market_chart/`,{
+                params:{
+                    vs_currency: "cad",
+                    days: "365",
                 },
             }),
         ]);
        
         setCoinData({
             day: formatData(day.data.prices),
-            week: formatData(week.data.prices)
+            week: formatData(week.data.prices),
+            year: formatData(year.data.prices)
         });
         setIsLoading(false);
         
@@ -57,7 +79,7 @@ const Coin = ({coin,id}) => {
                 data: {
                     datasets: [{
                         label: `${id} price`,
-                        data: coinData.day,
+                        data: determineTimeFormat(),
                         backgroundColor: "rgba(174,305,194,0.5)",
                         pointRadius:0,
                     }]
@@ -94,6 +116,11 @@ const Coin = ({coin,id}) => {
                 
                 <div className={style.coin_chart}>
                     <canvas ref={chartRef} id="myChart" width={550} height={550}></canvas>
+                    <div className={style.chart_button_section}>
+                    <button onClick={() => setTimeFormat("24h")} className={style.chart_button}>24h</button>
+                    <button  onClick={() => setTimeFormat("7d")} className={style.chart_button}>7d</button>
+                    <button    onClick={() => setTimeFormat("1y")} className={style.chart_button}>1y</button>
+                    </div>
                 </div>
                 <div className={style.coin_desc}>
                     <Image  src={coin.image.large} alt={coin.name} height={200} width={200} />
--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,9 @@ export default function Home({filteredCoins}) {
       </div>
 
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossOrigin="anonymous"></link>
-      
+      <footer >
+        <p className="created">Created by John Michael De Guzman</p>
+      </footer>
     </Layout>
   )
 }
